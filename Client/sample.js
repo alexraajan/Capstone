1b935409aaa0c713d245de77c33286560f06d654
Ramakrishnan <ramakrishnan4510@gmail.com>
2024-04-18 23:17:19 +1000

    UI Update

--- Client/sample.js
+++ Client/sample.js
@@ -1,6 +1,72 @@
+// Call this function when the page loads to populate the dropdown
+document.addEventListener('DOMContentLoaded', populateDistrictDropdown);
+
+async function populateDistrictDropdown() {
+    try {
+        const response = await fetch('http://127.0.0.1:5000/api/getSuburb');
+        const suburbs = await response.json();
+        const dropdown = document.getElementById('district-dropdown');
+        suburbs.forEach(suburbObject => {
+            const option = document.createElement('option');
+            option.value = suburbObject.Suburb.trim(); // Use trim to remove any whitespace
+            option.textContent = suburbObject.Suburb.trim(); // Use the key 'Town_suburb' to access the suburb name
+            dropdown.appendChild(option);
+        });
+		
+    } catch (error) {
+        console.error('Error fetching district names:', error);
+    }
+}
+
+function displayResponse(data) {
+    const schools = data.map(school => ({
+        School_name: school.School_name,
+        Level_of_schooling: school.Level_of_schooling,
+        Street: school.Street,
+        Postcode: school.Postcode,
+        Phone: school.Phone,
+        Website: school.Website
+    }));
+
+    const table = document.createElement('table');
+    const headerRow = table.insertRow();
+    for (const key in schools[0]) {
+        const headerCell = document.createElement('th');
+        headerCell.textContent = key.replace(/_/g, ' ');
+        headerRow.appendChild(headerCell);
+    }
+
+    schools.forEach(school => {
+        const row = table.insertRow();
+        for (const key in school) {
+            const cell = row.insertCell();
+            cell.textContent = school[key];
+        }
+    });
+
+    const jsonResponseContainer = document.getElementById('json-response');
+    jsonResponseContainer.innerHTML = '';
+    jsonResponseContainer.appendChild(table);
+}
+
+let isFirstSubmit = true;
 document.getElementById('search-form').addEventListener('submit', function(event) {
     event.preventDefault();
+    if (isFirstSubmit) {
+        // Uncollapse all elements with the id 'row'
+        document.querySelectorAll('#row-1').forEach(element => {
+            element.classList.remove('row-1');
+        });
+        document.querySelectorAll('#row-2').forEach(element => {
+            element.classList.remove('row-2');
+        });
+        document.querySelectorAll('#row-3').forEach(element => {
+            element.classList.remove('row-3');
+        });
+        isFirstSubmit = false; // Set the flag to false
+    }
     const keyword = document.getElementById('district-dropdown').value;
+    console.log(keyword);
     fetchData(keyword);
     fetchCrimeData(keyword);
 });
@@ -21,6 +87,10 @@ async function fetchData(keyword) {
     }
 }
 
+
+
+
+
 function displayResponse(data) {
     const schools = data.map(school => ({
         School_name: school.School_name,
@@ -32,7 +102,9 @@ function displayResponse(data) {
     }));
 
     const table = document.createElement('table');
+    table.classList.add('table','table-responsive', 'table-bordered','rounded-10', 'table-hover','rounded-table', 'text-center', 'text-capitalize');
     const headerRow = table.insertRow();
+    headerRow.classList.add('table-dark', 'table-active', 'text-uppercase', 'text-white')
     for (const key in schools[0]) {
         const headerCell = document.createElement('th');
         headerCell.textContent = key.replace(/_/g, ' ');
@@ -83,70 +155,12 @@ async function fetchCrimeData(suburb) {
     }
 }
 
-async function populateDistrictDropdown() {
-    try {
-        const response = await fetch('http://127.0.0.1:5000/api/getSuburb');
-        const suburbs = await response.json();
-        const dropdown = document.getElementById('district-dropdown');
-        suburbs.forEach(suburbObject => {
-            const option = document.createElement('option');
-            option.value = suburbObject.Suburb.trim(); // Use trim to remove any whitespace
-            option.textContent = suburbObject.Suburb.trim(); // Use the key 'Town_suburb' to access the suburb name
-            dropdown.appendChild(option);
-        });
-		 dropdown.addEventListener('change', function(event) {
-            const selectedSuburb = event.target.value;
-            // Now fetch the additional details based on selected suburb
-			console.log(selectedSuburb)
-            fetchDetailsForSuburb(selectedSuburb);
-        });
-    } catch (error) {
-        console.error('Error fetching district names:', error);
-    }
-}
-
-async function fetchDetailsForSuburb(suburb) {
-    const formData = new URLSearchParams();
-    formData.append('keyword', suburb);
-	console.log(suburb+"inside")
-    
-try {
-        const response = await fetch('http://127.0.0.1:5000/api/address', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-            body: formData
-        });
-        const details = await response.json();
-        console.log(details);
-
-        // Assume `details` is an array of address objects
-        const addressesDropdown = document.getElementById('addresses-dropdown');
-        addressesDropdown.innerHTML = ''; // Clear existing options
-        
-        details.forEach(address => {
-            const fullAddress = `${address["House No."]} ${address["Street"]}, ${address["Suburb"]} ${address["Postcode"]}`;
-			console.log(fullAddress);
-            const option = document.createElement('option');
-            option.value = fullAddress; // Here we're assigning the full address as the value
-            option.textContent = fullAddress; // And also as the text content that will be displayed
-            addressesDropdown.appendChild(option);
-        });
-    } catch (error) {
-        console.error('Error fetching details for suburb:', error);
-    }
-}
-
-// Call this function when the page loads to populate the dropdown
-document.addEventListener('DOMContentLoaded', populateDistrictDropdown);
-
-
 function drawLineChart(data, full_data) {
     const ctx = document.getElementById('myChart').getContext('2d');
     const labels = data.map(item => item['Offence category']);
     const counts = data.map(item => item['Dec-22']);
-    console.log(counts);
     const fullCounts = full_data.map(item => item['Dec-22']);
-    console.log(fullCounts);
+    
 
     const myLineChart = new Chart(ctx, {
         type: 'line',
